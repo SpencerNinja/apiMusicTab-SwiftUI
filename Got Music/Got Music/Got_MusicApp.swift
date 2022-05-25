4c431be64c3657127ae060a3a6b07641923175b8
SpencerNinja <62817123+SpencerNinja@users.noreply.github.com>
2022-05-25 13:36:17 -0600

    Initialize XCode project files

---
+++ Got Music/Got Music/Got_MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  Got_MusicApp.swift
+//  Got Music
+//
+//  Created by Spencer Hurd on 5/25/22.
+//
+
+import SwiftUI
+
+@main
+struct Got_MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
